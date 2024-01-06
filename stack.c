24367a7ae1ca5e03acb93f17c35c6308d03481af
kaustubhkharat <kakharat15@gmail.com>
2024-01-06 13:57:09 +0530

    created void stack files

---
+++ stack.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include "stack.h"
+
+/*
+typedef struct Node{
+	void *val;
+	struct Node *next;
+}Node;
+
+typedef Node *stack;
+*/
+
+void init(stack *s){
+	*s=NULL;
+	return;
+}
+
+void push(stack *s, void *val){
+	Node *nn;
+	nn=(Node*)malloc(sizeof(Node));
+	nn->val=val;
+	nn->next=*s;
+	*s=nn;
+	return;
+}
+
+void *pop(stack *s){
+	if (*s==NULL) return NULL;
+	void *val;
+	Node *p;
+
+	p=*s;
+	val=p->val;
+	*s=p->next;
+	free(p);
+	return val;
+}
+
+void *peek(stack *s){
+	if (*s==NULL) return NULL;
+	Node *p;
+	p=*s;
+	return p->val;
+}
---
+++ stack.h
@@ -0,0 +1,16 @@
+#ifndef STACK_H
+#define STACK_H 1
+
+typedef struct Node{
+	void *val;
+	struct Node *next;
+}Node;
+
+typedef Node *stack;
+
+void init(stack *s);
+void push(stack *s, void *val);
+void *pop(stack *s);
+void *peek(stack *s);
+
+#endif
